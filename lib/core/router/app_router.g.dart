64067706442844fb9d844987b730e66707c613d0
princek <princesamuelpks@gmail>
2026-01-19 19:39:52 +0000

    Create app_router.g.dart

---
+++ lib/core/router/app_router.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_router.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$goRouterHash() => r'2956d1e13768ef78f5043c39189011a342dc1d69';
+
+/// See also [goRouter].
+@ProviderFor(goRouter)
+final goRouterProvider = AutoDisposeProvider<GoRouter>.internal(
+  goRouter,
+  name: r'goRouterProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$goRouterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GoRouterRef = AutoDisposeProviderRef<GoRouter>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
