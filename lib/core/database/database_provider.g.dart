45a58cbf65291ca4809acbb0e20a25e7d14487b3
princek <princesamuelpks@gmail>
2026-01-19 19:39:48 +0000

    Create database_provider.g.dart

---
+++ lib/core/database/database_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'database_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$tasksBoxHash() => r'0e9834d5035e47931336ebf3dccaa960e406084b';
+
+/// See also [tasksBox].
+@ProviderFor(tasksBox)
+final tasksBoxProvider = Provider<Box<TaskModel>>.internal(
+  tasksBox,
+  name: r'tasksBoxProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$tasksBoxHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef TasksBoxRef = ProviderRef<Box<TaskModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
