b016340c12333b1c355fc0d121ccdd522db08992
princek <princesamuelpks@gmail>
2026-01-19 19:39:40 +0000

    Create app_colors.dart

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFF4A6EF6);
+  static const Color primaryDark = Color(0xFF3855C6);
+  static const Color primaryLight = Color(0xFFE8EEFF);
+
+  static const Color secondary = Color(0xFFFF9F69);
+  
+  static const Color backgroundLight = Color(0xFFF8F9FE);
+  static const Color backgroundDark = Color(0xFF121212);
+  
+  static const Color surfaceLight = Colors.white;
+  static const Color surfaceDark = Color(0xFF1E1E1E);
+
+  static const Color textPrimaryLight = Color(0xFF1C1C28);
+  static const Color textSecondaryLight = Color(0xFF8F9BB3);
+  
+  static const Color textPrimaryDark = Color(0xFFFFFFFF);
+  static const Color textSecondaryDark = Color(0xFFAAAAAA);
+
+  static const Color success = Color(0xFF00C48C);
+  static const Color error = Color(0xFFFF3B30);
+  static const Color warning = Color(0xFFFFCC00);
+
+  // Priority Colors
+  static const Color priorityHigh = Color(0xFFFF3B30);
+  static const Color priorityMedium = Color(0xFFFF9F69);
+  static const Color priorityLow = Color(0xFF00C48C);
+}
