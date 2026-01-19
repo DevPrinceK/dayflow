ebb4304a68c68cdfcb9edcddb4cdcd36e7ecacd7
princek <princesamuelpks@gmail>
2026-01-19 19:40:09 +0000

    Create task_repository_provider.g.dart

---
+++ lib/features/tasks/data/repositories/task_repository_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task_repository_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$taskRepositoryHash() => r'037c689a2cb1ded9785b31400fb0d777f1756a44';
+
+/// See also [taskRepository].
+@ProviderFor(taskRepository)
+final taskRepositoryProvider = AutoDisposeProvider<TaskRepository>.internal(
+  taskRepository,
+  name: r'taskRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$taskRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef TaskRepositoryRef = AutoDisposeProviderRef<TaskRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
